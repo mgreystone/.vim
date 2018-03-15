faf839c1fa6c21cb33e8488fcc5c418704f12772
Jasmina Jacquelina <jasminejacquelin@gmail.com>
2018-03-14 22:16:28 -0400

    added line to shell scripts to run PlugInstall immediately

--- neoVimInit.sh
+++ neoVimInit.sh
@@ -3,3 +3,5 @@ ln -s ~/.vim/init.vim ~/.config/nvim/
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+nvim -c 'PlugInstall | qa' #Install all Plugins!
--- vimInit.sh
+++ vimInit.sh
@@ -4,3 +4,4 @@ ln -s ~/.vim/init.vim ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+vim -c 'PlugInstall | qa' #Install all Plugins!
